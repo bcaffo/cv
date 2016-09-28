a2f67e64ab0c95b6ecbbf9bc384ca7c342cfc2f6
bcaffo <bcaffo@gmail.com>
2016-09-28 20:41:18 +0000

    auto build pdf

--- makecv.sh
+++ makecv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull
 pdflatex cv.tex
 bibtex cv1
 bibtex cv2
@@ -8,3 +9,5 @@ bibtex cv5
 #bibtex cv6
 pdflatex cv.tex
 pdflatex cv.tex
+git commit -a -m "auto build pdf"
+git push origin master

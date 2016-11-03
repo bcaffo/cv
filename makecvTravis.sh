87b30a4d7666e6129e6b048c954bb97700162964
Brian Caffo <bcaffo@users.noreply.github.com>
2016-11-03 09:15:06 -0400

    Added a file to make the cv and push changes back

---
+++ makecvTravis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+##this file compiles the CV for travis and pushes the results back to github
+##got some of this from : https://gist.github.com/willprice/e07efd73fb7f13f917ea
+
+## build the cv
+pdflatex cv.tex
+bibtex cv1
+bibtex cv2
+bibtex cv3
+bibtex cv4
+bibtex cv5
+#bibtex cv6
+pdflatex cv.tex
+pdflatex cv.tex
+
+##set up git
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git
+
+##commit and push 
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git push origin master
